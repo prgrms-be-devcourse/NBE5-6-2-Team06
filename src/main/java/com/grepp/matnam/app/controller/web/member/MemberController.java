3668464a815d3e730c69b51ddcc79fb1538bc251
Seojun Lee <leeseojun34@gmail.com>
2025-05-09 16:50:11 +0900

    chore: 엔티티 설계에 따른 이름 변경
    - member -> user로 변경

--- src/main/java/com/grepp/matnam/app/controller/web/member/MemberController.java
+++
@@ -1,9 +0,0 @@
-package com.grepp.matnam.app.controller.web.member;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class MemberController {
-
-
-}
---
+++ src/main/java/com/grepp/matnam/app/controller/web/user/UserController.java
@@ -0,0 +1,8 @@
+package com.grepp.matnam.app.controller.web.user;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+}
