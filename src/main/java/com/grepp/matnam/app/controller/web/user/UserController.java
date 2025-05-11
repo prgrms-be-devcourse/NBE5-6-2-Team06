702f036f4e516ef8d718f42abbade5ba0bb79912
Seojun Lee <leeseojun34@gmail.com>
2025-05-11 21:32:59 +0900

    feat: 기본적인 회원가입 컨트롤러 구현 및 페이지 구현

--- src/main/java/com/grepp/matnam/app/controller/web/user/UserController.java
+++ src/main/java/com/grepp/matnam/app/controller/web/user/UserController.java
@@ -1,9 +1,15 @@
 package com.grepp.matnam.app.controller.web.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
-
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "user/signup";
+    }
 }
