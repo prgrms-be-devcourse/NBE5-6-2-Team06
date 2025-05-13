bf677cd15f362a196a06643492ab26148de95fbc
JeonJW24 <tyui153@naver.com>
2025-05-14 00:01:33 +0900

    feat: 사용자 정지 관리 스케줄러 기능 구현

--- src/main/java/com/grepp/matnam/app/model/user/UserRepository.java
+++ src/main/java/com/grepp/matnam/app/model/user/UserRepository.java
@@ -1,6 +1,9 @@
 package com.grepp.matnam.app.model.user;
 
+import com.grepp.matnam.app.model.user.code.Status;
 import com.grepp.matnam.app.model.user.entity.User;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +15,5 @@ public interface UserRepository extends JpaRepository<User, String>, UserReposit
     Optional<User> findByUserId(String userId);
     boolean existsByEmail(String email);
     boolean existsByUserId(String userId);
+    List<User> findAllByStatusAndDueDateBefore(Status status, LocalDate date);
 }
