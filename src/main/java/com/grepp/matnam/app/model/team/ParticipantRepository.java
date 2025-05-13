0f44f411397c1ccb4d7d41f61593de84bce54d49
Seojun Lee <leeseojun34@gmail.com>
2025-05-14 00:59:11 +0900

    feat: 팀 리뷰를 위한 ParticipantRepository 기능 추가

--- src/main/java/com/grepp/matnam/app/model/team/ParticipantRepository.java
+++ src/main/java/com/grepp/matnam/app/model/team/ParticipantRepository.java
@@ -2,10 +2,21 @@ package com.grepp.matnam.app.model.team;
 
 import com.grepp.matnam.app.model.team.entity.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
 
 
     // userId와 teamId로 Participant 조회
     Participant findByUser_UserIdAndTeam_TeamId(String userId, Long teamId);
+
+    boolean existsByUser_UserIdAndTeam_TeamId(String userId, Long teamId);
+
+    List<Participant> findByTeam_TeamId(Long teamId);
+
+    @Query("SELECT p FROM Participant p JOIN FETCH p.user WHERE p.team.teamId = :teamId")
+    List<Participant> findParticipantsWithUserByTeamId(@Param("teamId") Long teamId);
 }
