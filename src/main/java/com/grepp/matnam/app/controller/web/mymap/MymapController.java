cd269d2e2f8a7c005db37a9620402a09583a1c69
hesador12 <97518677+hesador12@users.noreply.github.com>
2025-05-14 16:17:35 +0900

    feat: 맛집 추가 화면에서 기존 저장된 맛집 목록을 핀형태로 불러오는 기능을 구현했습니다.

--- src/main/java/com/grepp/matnam/app/controller/web/mymap/MymapController.java
+++ src/main/java/com/grepp/matnam/app/controller/web/mymap/MymapController.java
@@ -1,4 +1,4 @@
-package com.grepp.matnam.app.controller.web.mymap;
+package com.grepp.matnam.controller.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class MymapController {
 
-    // 맛집 등록 UI 페이지로 이동 (templates/map/add.html)
     @GetMapping("/map/add")
     public String showAddPage() {
         return "map/add";
