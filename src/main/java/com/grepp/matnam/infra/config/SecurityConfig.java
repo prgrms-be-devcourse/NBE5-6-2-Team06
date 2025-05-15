457aaebf7f5d92c4184e805530389eabdcd00297
Seojun Lee <leeseojun34@gmail.com>
2025-05-15 12:26:50 +0900

    feat: OAuth Authentication Success Handler 구현 및 SecurityConfig 설정

--- src/main/java/com/grepp/matnam/infra/config/SecurityConfig.java
+++ src/main/java/com/grepp/matnam/infra/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.grepp.matnam.infra.config;
 
+import com.grepp.matnam.app.model.user.CustomOauth2UserService;
 import com.grepp.matnam.infra.jwt.JwtAuthenticationFilter;
+import com.grepp.matnam.infra.oauth2.OAuth2AuthenticationSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -8,8 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -19,14 +19,15 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomOauth2UserService customOauth2UserService;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter,
+                          CustomOauth2UserService customOauth2UserService,
+                          OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        this.customOauth2UserService = customOauth2UserService;
+        this.oAuth2AuthenticationSuccessHandler = oAuth2AuthenticationSuccessHandler;
     }
 
     @Bean
@@ -41,6 +42,15 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 )
                 // #TODO: JWT 인증 추가
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(endpoint -> endpoint
+                                .userService(customOauth2UserService)
+                        )
+                        .redirectionEndpoint(endpoint -> endpoint
+                                .baseUri("/signin/oauth2/code/*")
+                        )
+                        .successHandler(oAuth2AuthenticationSuccessHandler)
+                )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
