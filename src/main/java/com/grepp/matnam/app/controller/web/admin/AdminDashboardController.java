51e5bcf82882df99dd72da33a21d7fe07f0cfa05
JeonJW24 <tyui153@naver.com>
2025-05-15 14:20:25 +0900

    feat: 관리자 [오늘 이용 회원] 기능 구현

--- src/main/java/com/grepp/matnam/app/controller/web/admin/AdminDashboardController.java
+++ src/main/java/com/grepp/matnam/app/controller/web/admin/AdminDashboardController.java
@@ -1,8 +1,9 @@
 package com.grepp.matnam.app.controller.web.admin;
 
+import com.grepp.matnam.app.controller.web.admin.payload.UserActivityLogResponse;
+import com.grepp.matnam.app.model.log.UserActivityLogService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +16,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //@PreAuthorize("hasRole('ADMIN')") // TODO : 모든 관리자 페이지 권한 설정 필요
 public class AdminDashboardController {
 
+    private final UserActivityLogService userActivityLogService;
+
     @GetMapping({"", "/", "/dashboard"})
     public String dashboard(Model model) {
+
+        UserActivityLogResponse todayLogStats = userActivityLogService.getTodayLogStats();
+
         model.addAttribute("pageTitle", "대시보드");
         model.addAttribute("currentPage", "dashboard");
-
-        // 여기에 대시보드 데이터를 모델에 추가
-        // 예: 총 회원 수, 오늘 서비스 이용 인원 등
+        model.addAttribute("todayLogStats", todayLogStats);
 
         return "admin/dashboard";
     }
