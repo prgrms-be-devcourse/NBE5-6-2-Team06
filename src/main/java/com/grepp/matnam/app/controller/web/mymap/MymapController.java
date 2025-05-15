4b4ca264afbb3dd3eee53f8656e40dbf39c8d046
hesador12 <97518677+hesador12@users.noreply.github.com>
2025-05-15 11:41:49 +0900

    feat: mypage내 띄워질 화면에 대한 매핑을 설정했습니다.

--- src/main/java/com/grepp/matnam/app/controller/web/mymap/MymapController.java
+++ src/main/java/com/grepp/matnam/app/controller/web/mymap/MymapController.java
@@ -1,15 +1,28 @@
 package com.grepp.matnam.app.controller.web.mymap;
 
+import com.grepp.matnam.app.model.user.UserService;
+import com.grepp.matnam.app.model.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class MymapController {
 
+    private final UserService userService;
+
     @GetMapping("/map/add")
     public String showAddPage() {
         return "map/add";
+    }
+
+    @GetMapping("/map/editMap")
+    public String showEditMapPage(Model model) {
+        String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userService.getUserById(userId);
+        return "map/editMap";
     }
 }
