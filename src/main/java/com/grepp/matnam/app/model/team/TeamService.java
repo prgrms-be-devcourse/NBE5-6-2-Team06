2fdc37878e5835a32e6f90b61e631eb6aae5ecaa
Park <ox2789@naver.com>
2025-05-19 22:20:40 +0900

    fix: header button 수정

--- src/main/java/com/grepp/matnam/app/model/team/TeamService.java
+++ src/main/java/com/grepp/matnam/app/model/team/TeamService.java
@@ -500,7 +500,6 @@ public class TeamService {
                 map(Map.Entry::getKey).
                 toList();
 
-            System.out.println(topKeywords);
             return topKeywords;
 
         }
