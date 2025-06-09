05feff61724723eeb7a8e4e3ddb6b55aee21ed5d
JeonJW24 <tyui153@naver.com>
2025-06-10 01:11:52 +0900

    refactor: request 유효성 검사

--- src/main/java/com/grepp/matnam/app/controller/api/admin/AdminRestaurantApiController.java
+++ src/main/java/com/grepp/matnam/app/controller/api/admin/AdminRestaurantApiController.java
@@ -15,7 +15,6 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
